3669f98aa6ba68c5009ebecb3b5fdf5248e4880c
narramuni <nmspavan@gmail.com>
2023-12-12 21:54:18 +0530

    first commit

---
+++ IISDockerDemo/Models/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IISDockerDemo.Models
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ IISDockerDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IISDockerDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
